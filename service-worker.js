debd2cc90f193f735a6b5fd2be99c27c703d542c
omorishaoba-alt <omorishaoba@gmail.com>
2026-05-10 17:25:52 +0100

    Add service worker for caching and offline support
    Implement service worker for caching files and handling fetch requests.

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,63 @@
+const CACHE_NAME = "comet-os-v9";
 
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(FILES);
+      })
+      .catch(err => {
+        console.log("CACHE INSTALL FAILED:", err);
+      })
+  );
+
+  self.skipWaiting();
+
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) {
+            return caches.delete(k);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    fetch(event.request)
+      .then(res => {
+
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, res.clone());
+          return res;
+        });
+
+      })
+      .catch(() => {
+        return caches.match(event.request)
+          .then(res => res || caches.match("./index.html"));
+      })
+  );
+
+});
