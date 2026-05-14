b97c297264421c2285ef2a3db86f687192989544
omorishaoba-alt <omorishaoba@gmail.com>
2026-05-15 00:31:40 +0100

    Add function to download PDF as text file

--- pdf.js
+++ pdf.js
@@ -1,1 +1,23 @@
+function downloadPDF(payload, reference) {
 
+  const content = `
+COMET OS TRANSACTION RECORD
+
+Reference: ${reference}
+
+STATUS: LOCKED
+
+PAYLOAD:
+${payload}
+  `;
+
+  const blob = new Blob([content], { type: "text/plain" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = reference + ".txt";
+  a.click();
+
+  URL.revokeObjectURL(url);
+}
